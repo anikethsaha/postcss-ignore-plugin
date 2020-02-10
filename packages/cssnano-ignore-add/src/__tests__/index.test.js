29f101a45d25757c90ab1cb1dfa3423a8d65944c
anikethsaha <anik220798@gmail.com>
2020-02-10 14:47:02 +0530

    chore: fix linting error

--- packages/cssnano-ignore-add/src/__tests__/index.test.js
+++ packages/cssnano-ignore-add/src/__tests__/index.test.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const helperPlugin = require('./helper/_testPlugin.js');
 const postcss = require('postcss');
+
 const plugin = require('..');
 
 function run(fixturename, data = []) {
