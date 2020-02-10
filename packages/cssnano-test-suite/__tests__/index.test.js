b2e55ce53ae4016a7d53e8412abb55be594448b0
anikethsaha <anik220798@gmail.com>
2020-02-10 19:56:26 +0530

    tests: added integration tests using cssnano

---
+++ packages/cssnano-test-suite/__tests__/index.test.js
@@ -0,0 +1,41 @@
+import fs from 'fs';
+import path from 'path';
+import postcss from 'postcss';
+import cssnano from 'cssnano';
+import cssnanoIgnoreRemove from '../../cssnano-ignore-remove';
+import cssnanoIgnoreAdd from '../../cssnano-ignore-add';
+
+function runWithDefaultPreset(fixturename) {
+  it(
+    'should match the snapshot for [' +
+      fixturename.replace('.css', '') +
+      '] using default cssnano preset',
+    (done) => {
+      const input = fs.readFileSync(
+        path.join(__dirname, `./fixtures/input/${fixturename}`),
+        'utf8'
+      );
+      postcss([
+        cssnanoIgnoreRemove(),
+        cssnano({
+          preset: 'default',
+        }),
+        cssnanoIgnoreAdd(),
+      ])
+        .process(input)
+        .then((result) => {
+          const output = fs.readFileSync(
+            path.join(__dirname, `./fixtures/output/${fixturename}`),
+            'utf8'
+          );
+
+          expect(result.css).toBe(output);
+          done();
+        });
+    }
+  );
+}
+
+fs.readdirSync(path.join(__dirname, './fixtures/input')).forEach(
+  runWithDefaultPreset
+);
